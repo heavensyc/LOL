beaa27430d18940fb2e5b5b6a4c9f62e2da49baa
mpark <mp.park@mail.utoronto.ca>
2017-03-13 19:54:58 -0400

    buttons

---
+++ script.js
@@ -0,0 +1,61 @@
+
+
+
+function summonerName() {
+  var summonername = prompt("Please enter your summoner name:","Enter name");
+  return summonername;
+}
+
+
+
+
+function leaveLink() {
+var exit = confirm("Do you really want to leave this page?");
+  if (exit==true){
+    alert("Thanks for visiting us!");
+  }
+  else{
+    alert("Thanks for staying!");
+  }
+}
+
+
+
+var x = 69;
+document.write(x);
+
+
+/*ternary operator
+var isAdult = (age < 18) ? "Too young": "Old enough";
+*/
+
+/*
+switch statements replace multiple else if statements
+var day = 2;
+switch (day) {
+  case 1:
+    document.write("Monday");
+    break;
+  case 2:
+    document.write("Tuesday");
+    break;
+  case 3:
+    document.write("Wednesday");
+    break;
+  default:
+    document.write("Another day");
+}
+Outputs "Tuesday"
+*/
+/*
+for loops consist of 3 componenets
+for (i=1; i<=5; i++) {
+   document.write(i + "<br />");
+}
+
+while loops
+do...while loops
+
+break statements
+continue statement
+*/
